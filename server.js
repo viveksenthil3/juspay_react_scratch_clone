7a8de4daaaf08ec0f0bba419484db73466c4386b
viveksenthil3 <svivek.hosur5@gmail.com>
2021-07-15 01:23:23 +0530

    production config done

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 8000;
+const app = express();
+
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname+'\\public'));
+
+// send the user to index html page inspite of the url
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname+'\\public', 'index.html'));
+});
+
+app.listen(port, ()=>{
+    console.log(`Listening on port : ${port}`)
+});
